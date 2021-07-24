14b9a2e4f81653c23ed98415276cbaa703dacb78
RFHK-CODE <87824187+RFHK-CODE@users.noreply.github.com>
2021-07-24 11:18:40 +0500

    Add files via upload

--- JavaScript/Toggle.js
+++ JavaScript/Toggle.js
@@ -4,23 +4,16 @@ searchBar = document.querySelector(".search-bar")
 navbar = document.querySelector(".bar")
 leftNavList = document.querySelector(".left-nav-list")
 toggle.addEventListener('click',()=>{
-    if (searchBar.classList.value != "search-bar") {
-        navbar.classList.toggle("media-bar")
-        leftNavList.classList.toggle("media-list")
-    }
-    else {
-        navbar.classList.toggle("media-bar")
-        leftNavList.classList.toggle("media-list")
+    if (!(searchBar.classList.value != "search-bar")) { 
         searchBar.classList.toggle("ssearch-bar") 
     }
+    navbar.classList.toggle("media-bar")
+    leftNavList.classList.toggle("media-list")
 })
 toggleSearch.addEventListener('click',()=>{
-    if (navbar.classList.value != "bar") {
-        searchBar.classList.toggle("ssearch-bar") 
-    }
-    else {
+    if (!(navbar.classList.value != "bar")) {
         navbar.classList.toggle("media-bar")
         leftNavList.classList.toggle("media-list")
-        searchBar.classList.toggle("ssearch-bar") 
     }
+    searchBar.classList.toggle("ssearch-bar")
 })
